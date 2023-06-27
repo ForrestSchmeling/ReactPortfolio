9a0295fec64df696151ccd0ead2dfab1fdf253b7
Forrest Schmeling <forrestschmeling@icloud.com>
2023-06-27 10:07:14 +0000

    Created react-app portfolio , added router, material ui, and vertical timeline, also started adding some routes to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import {BrowserRouter as router,Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-       
+       <Router>
+        <Routes>
+          <Route path = "/" element ={<Home />} />
+        </Routes>
+       </Router>
     </div>
   );
 }

04f6f255ceee19713544879fa3c5da2835a19745
Forrest Schmeling <forrestschmeling@icloud.com>
2023-06-28 09:09:26 +0000

    added a skills section that was taken from the homepage originally.

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -11,27 +11,6 @@ function Home() {
 
         <div className='prompt'> <p> A Software Developer with a passion for learning and creating code. </p></div>
       </div>
-      <div className='skills'>
-        <h1>Skills:</h1>
-        <ol className='list'>
-          <li className='item'> 
-          <h2>Front-End</h2>
-          <span>HTML,CSS, JavaScript, ReactJS, Bootstrap</span>
-          </li>
-          <li className='item'> 
-          <h2>Back-End</h2>
-          <span>SQL, .Net, Java, C++, Python, Ruby, C#</span>
-          </li>
-          <li className='item'> 
-          <h2>Technical Skills</h2>
-          <span>SDLC, Scrum, Agile, Waterfall, Object-Oriented Programming, Mobile Applications, Data Structures & Algorithms.</span>
-          </li>
-          <li className='item'> 
-          <h2>Certifications</h2>
-          <span>CompTIA A+ Certified, CompTIA Project+ Certified, and ITIL 4 Foundations Certified.</span>
-          </li>
-        </ol>
-         </div>
     </div>
   )
 }

6e2e593baaceaf77ec8c9ed0a3d53e6c834d0370
Forrest Schmeling <forrestschmeling@icloud.com>
2023-07-01 06:22:28 +0000

    removed my subfolder and put the project in the root folder, also removed the typo in the skills section.

---
+++ src/pages/Experience.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "../Styles/experience.css"
+
+function Experience() {
+  return (
+    <div className='experience'>
+    <div className='skills'>
+      <h1>Skills</h1>
+    <ol className='list'>
+      <li className='item'> 
+      <h2>Front-End</h2>
+      <span>HTML,CSS, JavaScript, ReactJS, Bootstrap</span>
+      </li>
+      <li className='item'> 
+      <h2>Back-End</h2>
+      <span>SQL, .Net, Java, C++, Python, Ruby, C#</span>
+      </li>
+      <li className='item'> 
+      <h2>Technical Skills</h2>
+      <span>SDLC, Scrum, Agile, Waterfall, Object-Oriented Programming, Mobile Applications, Data Structures & Algorithms.</span>
+      </li>
+      <li className='item'> 
+      <h2>Certifications</h2>
+      <span>CompTIA A+ Certified, CompTIA Project+ Certified, and ITIL 4 Foundations Certified.</span>
+      </li>
+      <h1>College:</h1>
+      <li className='item'>
+       <h2>Western Governors University </h2>
+       <span>
+Bachelor of Science - BS, Software Development Jul 2022 - Oct 2023
+</span>
+      </li>
+    </ol>
+     </div>
+  </div>
+  )
+}
+
+export default Experience
